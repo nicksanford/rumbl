02c0a17b7fd880348ead9723af354a7b94281e2f
Nick Sanford <nicholascsanford@gmail.com>
2018-11-06 22:29:33 -0500

    Complete chapter 5 - auth

--- lib/rumbl/accounts/accounts.ex
+++ lib/rumbl/accounts/accounts.ex
@@ -3,11 +3,38 @@ defmodule Rumbl.Accounts do
   The Accounts context.
   """
 
-  import Ecto.Query, warn: false
+  import Ecto.Query
   alias Rumbl.Repo
 
   alias Rumbl.Accounts.User
   alias Rumbl.Accounts.Credential
+
+  def get_user_by_email(email) do
+    from(u in User,
+      join: c in assoc(u, :credential),
+      where: c.email == ^email)
+    |> Repo.one()
+    |> Repo.preload(:credential)
+  end
+
+  def authenticate_by_email_and_pass(email, given_pass) do
+    user = get_user_by_email(email)
+    cond do
+      user && Comeonin.Pbkdf2.checkpw(given_pass, user.credential.password_hash) ->
+        {:ok, user}
+      user ->
+        {:error, :unauthorized}
+      true ->
+        Comeonin.Bcrypt.dummy_checkpw()
+        {:error, :not_found}
+    end
+  end
+
+  def register_user(attrs \\ %{}) do
+    %User{}
+    |> User.registration_changeset(attrs)
+    |> Repo.insert()
+  end
 
   def change_user(%User{} = user) do
     User.changeset(user, %{})
