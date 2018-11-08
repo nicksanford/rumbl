616b860af7e09ffe7c001b355487fe008358929e
Nick Sanford <nicholascsanford@gmail.com>
2018-11-08 09:00:05 -0500

    Chapter 10

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import css from "../css/app.css"
+import css from "../css/app.css";
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -9,7 +9,7 @@ import css from "../css/app.css"
 //
 // Import dependencies
 //
-import "phoenix_html"
+import "phoenix_html";
 
 // Import local files
 //
@@ -17,3 +17,11 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+import Player from "./player";
+let video = document.getElementById("video");
+
+if (video) {
+    Player.init(video.id, video.getAttribute("data-player-id"), () => {
+        console.log("player ready!");
+    });
+}
